a22e2597fc56d4f300512892bebac597181d0d5f
shahriar-shakil <toshahriarshakil@gmail.com>
2024-08-12 04:38:47 +0600

    working with string

--- giraffe.rb
+++ giraffe.rb
@@ -11,7 +11,14 @@ puts ("Her name is " + person_name)
 puts ("she is " + person_age + " years old.")
 
 # data types 
-name = "shahriar" 
+name = "shahriar shakil" 
 age = nil
 gpa = 3.2
 isMale = true
+# working with string
+
+puts name.upcase()
+puts name.strip()
+puts name.length()
+puts name.include? "shakil"
+puts "programming".upcase()
