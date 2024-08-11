2cceed77f135837b4674c17fee29c3e758860456
shahriar-shakil <toshahriarshakil@gmail.com>
2024-08-12 04:29:33 +0600

    variables and data type

--- giraffe.rb
+++ giraffe.rb
@@ -1,4 +1,17 @@
-puts "shahriar"
-puts " is happy"
-print "Hello world"
-print "Hello world"
+# puts "shahriar"
+# puts " is happy"
+# print "Hello world"
+# print "Hello world"
+
+#  variables 
+person_name = "Nitu"
+person_age = "25"
+
+puts ("Her name is " + person_name)
+puts ("she is " + person_age + " years old.")
+
+# data types 
+name = "shahriar" 
+age = nil
+gpa = 3.2
+isMale = true
