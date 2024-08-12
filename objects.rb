dd89ba4d5d319be63256213a2ddf1b7e8fd8d6c9
shahriar-shakil <toshahriarshakil@gmail.com>
2024-08-12 23:58:03 +0600

    class and object

---
+++ objects.rb
@@ -0,0 +1,18 @@
+# class Book
+#  attr_accessor :title, :author, :pages
+
+# end
+
+# book1 = Book.new()
+# book1.title = "Amar Dekha Rajnitir 50 bosor"
+# book1.pages = "500"
+# book1.author = "Abul Monsur Ahmed"
+
+# puts book1.title
+
+# book2 = Book.new()
+# book2.title = "Kopal kundula"
+# book2.pages = "250"
+# book2.author = "Bongkim"
+
+# puts book2.author
