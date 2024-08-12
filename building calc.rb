55444611966b5c0d7b46f89fb7bb25d8cbd5a7d5
shahriar-shakil <toshahriarshakil@gmail.com>
2024-08-12 15:04:45 +0600

    working with operator in calculator

--- building calc.rb
+++ building calc.rb
@@ -1,7 +1,21 @@
 puts "Enter a number: "
-num1 = gets.chomp()
+num1 = gets.chomp().to_f
 
-puts "Enter another number: "
-num2 = gets.chomp()
+puts "Enter operator: "
+op = gets.chomp()
 
-puts (num1.to_i + num2.to_i) 
+puts "Enter second number: "
+num2 = gets.chomp().to_f
+
+if op == "+"
+  puts (num1  + num2)
+  elsif op == "-"
+  puts (num1  - num2)
+elsif op == "/"
+  puts (num1  / num2)
+elsif op == "*"
+  puts (num1  * num2)
+elsif op == "%"
+  puts (num1  % num2)
+else puts "invalid operator"
+end
