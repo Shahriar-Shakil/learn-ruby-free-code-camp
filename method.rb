072678b79ca11ce6ae9426f0870b69da19f2f8fd
shahriar-shakil <toshahriarshakil@gmail.com>
2024-08-12 14:29:10 +0600

    return statement

--- method.rb
+++ method.rb
@@ -1,7 +1,18 @@
-def sayhi(name)
-  puts "hello " +  name
-end
 
-puts "top"
-sayhi("Shahriar")
-puts "bottom"
+# def sayhi(name)
+#   puts "hello " +  name
+# end
+
+# puts "top"
+# sayhi("Shahriar")
+# puts "bottom"
+
+# return statement
+
+# def cube(num)
+# return num * num * num
+# end
+
+# puts cube(2)
+
+# if statement 
