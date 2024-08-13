443388afde58d5d3598c8128e601e39a6c57c152
shahriar-shakil <toshahriarshakil@gmail.com>
2024-08-13 18:41:00 +0600

    objects method

--- objects.rb
+++ objects.rb
@@ -16,3 +16,25 @@
 # book2.author = "Bongkim"
 
 # puts book2.author
+
+class Book
+  attr_accessor :title, :author, :pages
+  def initialize(title,author,pages)
+  @title = title
+  @author = author
+  @pages = pages
+  end
+
+  def is_big_book
+    if @pages >= 400 
+      return true
+    end
+    return false
+  end
+end
+ book1 = Book.new("Harry Poter","JK Rowling",500)
+ book2 = Book.new("Himu","Humaun Ahmed",500)
+ book3 = Book.new("Himu1","Humaun Ahmed",200)
+puts book1.title
+puts book2.author
+puts book3.is_big_book
